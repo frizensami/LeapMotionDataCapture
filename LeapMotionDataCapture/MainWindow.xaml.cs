fd93d91ef8781745a4f361b6db66bc94485f32de
Sriram Sami <frizensami@gmail.com>
2015-04-06 11:09:50 +0800

    Initial Commit of basic leapmotion project

---
+++ LeapMotionDataCapture/MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Leap;
+using System.Diagnostics;
+
+namespace LeapMotionDataCapture
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, ILeapEventDelegate
+    {
+        //overall controller for the leap motion device
+        private Controller controller = new Controller();
+
+        //instance of a listener that is called when events happen in the leap device
+        private LeapEventListener listener;
+
+        //checks if program is closing
+        private Boolean isClosing = false;
+
+        /// <summary>
+        /// Init vars and subscribe the required listener to the leap controller
+        /// </summary>
+        /// 
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+        }
+
+        
+        delegate void LeapEventDelegate(string EventName);
+        public void LeapEventNotification(string EventName)
+        {
+            if (this.CheckAccess())
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+                    case "onConnect":
+                        this.connectHandler();
+                        break;
+                    case "onFrame":
+                        if (!this.isClosing)
+                            this.newFrameHandler(this.controller.Frame());
+                        break;
+                }
+            }
+            else
+            {
+                Dispatcher.Invoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+
+        void connectHandler()
+        {
+            this.controller.SetPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            this.controller.Config.SetFloat("Gesture.Swipe.MinLength", 100.0f);
+        }
+
+        void newFrameHandler(Leap.Frame frame)
+        {
+            this.lblID.Content = frame.Id.ToString();
+            this.lblTimestamp.Content = frame.Timestamp.ToString();
+            this.lblFPS.Content = frame.CurrentFramesPerSecond.ToString();
+            this.lblIsValid.Content = frame.IsValid.ToString();
+            this.lblGestureCount.Content = frame.Gestures().Count.ToString();
+            this.lblImageCount.Content = frame.Images.Count.ToString();
+        }
+
+        void MainWindow_Closing(object sender, EventArgs e)
+        {
+            this.isClosing = true;
+            this.controller.RemoveListener(this.listener);
+            this.controller.Dispose();
+        }
+    }
+
+    /// <summary>
+    /// Interface that requires interfacer to have the function leapeventnotification that ac
+    /// </summary>
+    public interface ILeapEventDelegate
+    {
+        void LeapEventNotification(string EventName);
+    }
+
+    public class LeapEventListener :  Listener
+    {
+        ILeapEventDelegate eventDelegate;
+
+        public LeapEventListener(ILeapEventDelegate delegateObject)
+        {
+            this.eventDelegate = delegateObject;
+        }
+        public override void OnInit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onInit");
+        }
+        public override void OnConnect(Controller controller)
+        {
+            controller.SetPolicy(Controller.PolicyFlag.POLICY_IMAGES);
+            controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+            this.eventDelegate.LeapEventNotification("onConnect");
+        }
+
+        public override void OnFrame(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onFrame");
+        }
+        public override void OnExit(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onExit");
+        }
+        public override void OnDisconnect(Controller controller)
+        {
+            this.eventDelegate.LeapEventNotification("onDisconnect");
+        }
+
+    }
+}
